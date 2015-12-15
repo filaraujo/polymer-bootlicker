dfc2f11d06c883d1a35152f15fe1967019fe7939
Filipe Araujo <filipe.alves.araujo@gmail.com>
2015-12-15 11:33:03 -0500

    adding sourcemaps

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -1,16 +1,20 @@
 import babel from 'gulp-babel';
 import gulp from 'gulp';
 import {paths} from '../tasks/paths.js';
+import sourcemaps from 'gulp-sourcemaps';
 import size from 'gulp-size';
 import uglify from 'gulp-uglify';
 
 const babelConfig = {presets: ['es2015']};
+const sourcemapsConfig = ['.', {}];
 const uglifyConfig = {preserveComments: 'some'};
 
 export function scripts() {
   return gulp.src(paths.scripts)
+    .pipe(sourcemaps.init())
     .pipe(babel(babelConfig))
     .pipe(uglify(uglifyConfig))
+    .pipe(sourcemaps.write(...sourcemapsConfig))
     .pipe(size({title: 'scripts'}))
     .pipe(gulp.dest(paths.dist));
 }
