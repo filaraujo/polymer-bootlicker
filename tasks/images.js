4e88129f892987b0dad8113a16211c3c866804f3
Filipe Araujo <filipe.alves.araujo@gmail.com>
2015-12-15 11:06:32 -0500

    adding uglify

--- tasks/images.js
+++ tasks/images.js
@@ -1,13 +1,13 @@
 import gulp from 'gulp';
 import imagemin from 'gulp-imagemin';
 import {paths} from '../tasks/paths.js';
+import size from 'gulp-size';
+
+const imageminConfig = {progressive: true, interlaced: true};
 
 export function images() {
   return gulp.src(paths.images)
-    // optimize
-    .pipe(imagemin({
-      progressive: true,
-      interlaced: true
-    }))
+    .pipe(imagemin(imageminConfig))
+    .pipe(size({title: 'images'}))
     .pipe(gulp.dest(paths.dist));
 }
--- tasks/scripts.js
+++ tasks/scripts.js
@@ -1,9 +1,16 @@
-import gulp from 'gulp';
 import babel from 'gulp-babel';
+import gulp from 'gulp';
 import {paths} from '../tasks/paths.js';
+import size from 'gulp-size';
+import uglify from 'gulp-uglify';
+
+const babelConfig = {presets: ['es2015']};
+const uglifyConfig = {preserveComments: 'some'};
 
 export function scripts() {
   return gulp.src(paths.scripts)
-    .pipe(babel({presets: ['es2015']}))
+    .pipe(babel(babelConfig))
+    .pipe(uglify(uglifyConfig))
+    .pipe(size({title: 'scripts'}))
     .pipe(gulp.dest(paths.dist));
 }
