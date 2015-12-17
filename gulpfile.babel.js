b687e9872da69649b21e8640055ad56ffd35002f
Filipe Araujo <filipe.alves.araujo@gmail.com>
2015-12-16 22:41:10 -0500

    clean up

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,11 +8,17 @@ import {scripts} from './tasks/scripts';
 import {reload, serve} from './tasks/server';
 import {styles} from './tasks/styles';
 
+/**
+ * clean out dist folder
+ */
 function clean() {
-  console.log('clean');
   return del('./dist');
 }
 
+/**
+ * watch for font, images, scripts and styles changes
+ * then reprocess and reload
+ */
 function watch() {
   let watchableTasks = [fonts, images, scripts, styles];
 
@@ -30,17 +36,17 @@ let build = gulp.series(
   processAssets
 );
 
-let localServe = gulp.series(
+let serveLocal = gulp.series(
   build,
   serveAndWatch
 );
 
-let localDist = gulp.series(
+let serveDist = gulp.series(
   build,
   useReferences,
   serveAndWatch
 );
 
 gulp.task('build', build);
-gulp.task('serve', localServe);
-gulp.task('serve:dist', localDist);
+gulp.task('serve', serveLocal);
+gulp.task('serve:dist', serveDist);
