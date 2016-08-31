da479a57131c79d87c3a2d95e435c26eba0887be
Filipe Araujo <filipe.alves.araujo@gmail.com>
2016-08-31 19:18:19 -0400

    remove autoprefixer and let cssnano handle it for me

--- src/registries/style.js
+++ src/registries/style.js
@@ -1,23 +1,26 @@
 import util from 'util';
-import autoprefixer from 'gulp-autoprefixer';
 import nano from 'gulp-cssnano';
 import Registry from 'undertaker-registry';
 import size from 'gulp-size';
 import sourcemaps from 'gulp-sourcemaps';
 import through from 'through2';
 
-const autoprefixerConfig = [
-  'ie >= 10',
-  'ie_mob >= 10',
-  'ff >= 30',
-  'chrome >= 34',
-  'safari >= 7',
-  'opera >= 23',
-  'ios >= 7',
-  'android >= 4.4',
-  'bb >= 10'
-];
-const nanoConfig = {};
+const nanoConfig = {
+  autoprefixer: {
+    add: true,
+    browsers: [
+      'ie >= 10',
+      'ie_mob >= 10',
+      'ff >= 30',
+      'chrome >= 34',
+      'safari >= 7',
+      'opera >= 23',
+      'ios >= 7',
+      'android >= 4.4',
+      'bb >= 10'
+    ]
+  }
+};
 const sourcemapsConfig = ['.', {}];
 
 /**
@@ -55,10 +58,8 @@ function StyleRegistry(config = {}) {
 
     return taker.src(paths.styles, {base: paths.app})
       .pipe(preprocess())
-      .pipe(autoprefixer(autoprefixerConfig))
       .pipe(sourcemaps.init())
       .pipe(nano(nanoConfig))
-      .pipe(sourcemaps.init())
       .pipe(sourcemaps.write(...sourcemapsConfig))
       .pipe(postprocess())
       .pipe(size({title: 'styles'}))
